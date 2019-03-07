f7df2cde230c0d3795d1f03ed833b3bf83571b99
fabianleyton <fabianlock@hotmail.com>
2019-03-06 21:49:07 -0500

    some test

--- view-organization.js
+++ view-organization.js
@@ -7,19 +7,21 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry(1,1,1);
+var geometry = new THREE.BoxGeometry(1,2,1);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+//camera.position.y = 2;
 
 function animate(){
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
+    cube.rotation.z +=0.01;
 }
 
 
