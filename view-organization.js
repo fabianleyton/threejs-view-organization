ae4b63097b5db16ab2e9662cc1bcb7859cde9d9f
fabianleyton <fabianlock@hotmail.com>
2019-03-07 22:05:53 -0500

    change for clas

--- view-organization.js
+++ view-organization.js
@@ -1,29 +1,41 @@
 /* Fabian Leyton */
+var ViewOrganization = ViewOrganization || {};
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+ViewOrganization.init = () =>{
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.BoxGeometry(1,2,1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-//camera.position.y = 2;
-
-function animate(){
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-
-    //cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
-    cube.rotation.z +=0.01;
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    var arrayNode = [];
+    
+    
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    
+    var geometry = new THREE.BoxGeometry(1,2,1);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
+    camera.position.z = 5;
+    //camera.position.y = 2;
+    
+    function animate(){
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    
+        //cube.rotation.x += 0.01;
+        //cube.rotation.y += 0.01;
+        cube.rotation.z +=0.01;
+    }
+    animate();
+    
 }
 
-
-
-animate();
+ViewOrganization.addNode = (id, text, color) => {
+    arrayNode.push({
+        "id": id,
+        "text": text,
+        "color" : color,
+    })
+}
